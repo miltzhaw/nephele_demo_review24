d25387ba5acaf984f7adc6ce5d1767d34b991728
miltzhaw <miltzhaw@zhaw.ch>
2024-03-22 15:02:00 +0100

    tested version

--- app/app.py
+++ app/app.py
@@ -22,17 +22,19 @@ def trigger_execution():
     result = asyncio.run(trigger())
     print("trigger", result)
     # return f"<p>{result}</p>"
-    try:
-        # Simulate execution by making an HTTP call
-        response = requests.get('https://jsonplaceholder.typicode.com/posts/1')
-        if response.status_code == 200:
-            execution_status = "Success"
-        else:
-            execution_status = "Failed"
-    except Exception as e:
-        execution_status = "Failed: " + str(e)
     
-    return render_template('index.html', execution_status=execution_status)
+    #try:
+        # Simulate execution by making an HTTP call
+    #    response = requests.get('https://jsonplaceholder.typicode.com/posts/1')
+    #    if response.status_code == 200:
+    #        execution_status = "Success"
+    #    else:
+    #        execution_status = "Failed"
+   # except Exception as e:
+   #     execution_status = "Failed: " + str(e)
+    
+   # return render_template('index.html', execution_status=execution_status)
+    return render_template('index.html', execution_status=result)
 
 async def trigger():
     http_client = HTTPClient()
@@ -44,7 +46,7 @@ async def trigger():
     }
     http_client.set_security(security_scheme_dict, credentials_dict)
     wot = WoT(servient=Servient(clients=[http_client]))
-    consumed_thing = await wot.consume_from_url("http://vo-tb2:9090/vo-tb2")
+    consumed_thing = await wot.consume_from_url("http://vo-tb2:30886/vo-tb2")
     result = await consumed_thing.invoke_action("triggerBringup", {'launchfileId': desired_launch_file_id }) # desired_launch_file_id=[bringup, startmapping, saveMap]
     print(result)
     return result
@@ -54,14 +56,15 @@ def read_data():
     result = asyncio.run(read())
     print("read", result)
     # return f"<p>{result}</p>"
-    try:
+   # try:
         # Simulate reading data by making an HTTP call
-        response = requests.get('https://jsonplaceholder.typicode.com/posts/1')
-        data = response.json()
-    except Exception as e:
-        data = {"error": str(e)}
+  #      response = requests.get('https://jsonplaceholder.typicode.com/posts/1')
+  #      data = response.json()
+   #  except Exception as e:
+   #     data = {"error": str(e)}
     
-    return render_template('index.html', data=data)
+   # return render_template('index.html', data=data)
+    return render_template('index.html', data=result)
 
 async def read():
     http_client = HTTPClient()
@@ -73,7 +76,7 @@ async def read():
     }
     http_client.set_security(security_scheme_dict, credentials_dict)
     wot = WoT(servient=Servient(clients=[http_client]))
-    consumed_thing = await wot.consume_from_url("http://vo-tb2:9090/vo-tb2")
+    consumed_thing = await wot.consume_from_url("http://vo-tb2:30886/vo-tb2")
     result = await consumed_thing.properties["allAvailableResources"].read()
     return result
 
--- foxglove-zenoh-bridges/run_bridge.sh
+++ foxglove-zenoh-bridges/run_bridge.sh
@@ -3,6 +3,8 @@ source /root/colcon_ws/install/setup.bash
 ros2 launch foxglove_bridge foxglove_bridge_launch.xml &
 BRIDGE_PID=$!
 
+sleep 30
+
 ./zenoh-bridge-ros2dds -c zenoh-bridge-conf.json5 &
 ZENOH_PID=$!
 
